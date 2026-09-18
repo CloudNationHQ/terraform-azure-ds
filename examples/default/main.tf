7ba644b034b33e9cd6aaee848e2a82730d733d7c
dkooll <dennis.kool@cloudnation.nl>
2026-09-18 11:54:51 +0200

    feat: azurerm provider 5 upgrade BREAKING CHANGE: this change causes recreates

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,13 +1,13 @@
 module "naming" {
   source  = "cloudnationhq/naming/azure"
-  version = "~> 0.26"
+  version = "~> 0.32"
 
   suffix = ["demo", "dev"]
 }
 
 module "rg" {
   source  = "cloudnationhq/rg/azure"
-  version = "~> 2.0"
+  version = "~> 3.0"
 
   groups = {
     demo = {
@@ -19,16 +19,12 @@ module "rg" {
 
 module "ds" {
   source  = "cloudnationhq/ds/azure"
-  version = "~> 1.0"
+  version = "~> 2.0"
 
   location            = module.rg.groups.demo.location
   resource_group_name = module.rg.groups.demo.name
 
   account = {
     name = module.naming.data_share_account.name_unique
-  }
-
-  tags = {
-    environment = "demo"
   }
 }
